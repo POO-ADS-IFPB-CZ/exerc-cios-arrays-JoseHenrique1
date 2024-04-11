4ab2bd590da26fd1fae3beb98af162e6d8595bdc
Jose Henrique <jh4832184@gmail.com>
2024-04-11 10:15:55 -0300

    feat: add question three

--- src/App.java
+++ src/App.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         System.err.println("Questão 2");
         fibonacci(0, 1, 30);
+
+        System.err.println("Questão 3");
+        grade(5);
     }
 
     //valores da questão               0               1               30
@@ -18,5 +23,40 @@ public class App {
         } 
     }
 
+    public static void grade(int lengthArray) {
+        double grades[] = new double[lengthArray];
+        int counter = 1;
+        Scanner scanner = new Scanner(System.in);
+
+        //Recebendo as notas
+        while (counter <= lengthArray) {
+            System.out.print("Digite sua "+counter+"° nota: ");
+            grades[counter-1] = scanner.nextDouble();
+            scanner.nextLine();
+            ++counter;
+        }
+
+        //media recebe todas as notas, depois divide pelo tamanho
+        double averageGrade = 0;
+        for (double grade : grades) {
+            averageGrade += grade;
+        }
+        averageGrade /= (double) lengthArray;
+
+        int biggerNotes = 0;
+        int minorNotes = 0;
+
+        //calcula as notas maiores e menores que a media
+        for (double grade : grades) {
+            if (grade > averageGrade) {
+                ++biggerNotes;
+            }
+            if (grade < averageGrade) {
+                ++minorNotes;
+            }
+        }
+
+        System.out.printf("Media: %f \nNotas maiores que a media: %d \nNotas menores que a media: %d", averageGrade, biggerNotes, minorNotes);
+    }
 
 }
